af55ff25da84675b06eac5ffa286170ef4087334
Arkadi <oiarkadi@gmail.com>
2024-10-23 08:48:38 -0700

    first

---
+++ js/buttons.js
@@ -0,0 +1,52 @@
+function redirectToUrl() {
+  window.location.href = "https://www.google.com";
+}
+
+document.getElementById("get-free-api-btn")
+  .addEventListener("click", redirectToUrl);
+
+
+let words = document.querySelectorAll(".word");
+words.forEach((word) => {
+  let letters = word.textContent.split("");
+  word.textContent = "";
+  letters.forEach((letter) => {
+    let span = document.createElement("span");
+    if (letter === " ") {
+      span.innerHTML = "&nbsp;"; // Use non-breaking space for spaces
+    } else {
+      span.textContent = letter;
+    }
+    span.className = "letter";
+    word.append(span);
+  });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let rotateText = () => {
+  let currentWord = words[currentWordIndex];
+  let nextWord =
+    currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+  // Rotate out letters of current word
+  Array.from(currentWord.children).forEach((letter, i) => {
+    setTimeout(() => {
+      letter.className = "letter out";
+    }, i * 80);
+  });
+  // Reveal and rotate in letters of next word
+  nextWord.style.opacity = "1";
+  Array.from(nextWord.children).forEach((letter, i) => {
+    letter.className = "letter behind";
+    setTimeout(() => {
+      letter.className = "letter in";
+    }, 340 + i * 80);
+  });
+  currentWordIndex =
+    currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+rotateText();
+setInterval(rotateText, 4000);
